cea1c7cff9e7fe6ed057c2909624b169a1d4fb30
Esteban Hernandez <hernandez.dev.services@gmail.com>
2019-05-27 13:35:41 -0400

    NGINX Reverse Proxy
    Installed the NGINX reverse proxy and added three mock services:
    - 404 is the default service for any unmatched request.
    - posts and albums are simple JSON endpoints for mocking actual servers.

--- mock-json-service/app.py
+++ mock-json-service/app.py
@@ -2,15 +2,29 @@ import requests as rqs
 from flask import Flask, jsonify
 from flask_api import status
 import json
+import os
 
 app = Flask('mock-json-service')
 
-POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+RETURN_404 = False
+
+if 'RESOURCE' in os.environ:
+    RESOURCE_NAME = os.environ['RESOURCE']
+    if RESOURCE_NAME == 'posts':
+        RESOURCE_URL = 'https://jsonplaceholder.typicode.com/posts'
+    elif RESOURCE_NAME == 'albums':
+        RESOURCE_URL = 'https://jsonplaceholder.typicode.com/albums'
+    else:
+        RETURN_404 = True
+else:
+    RETURN_404 = True
 
 @app.route('/')
-def posts():
-    res = rqs.get(POSTS_URL)
+def resources():
+    if RETURN_404:
+        return jsonify({ 'message': 'Resource not found.'}), status.HTTP_404_NOT_FOUND
+    res = rqs.get(RESOURCE_URL)
     if res.status_code != 200:
-        return jsonify({ 'message': 'Unable to fetch posts.' }), status.HTTP_500_INTERNAL_SERVER_ERROR 
-    posts = json.loads(res.content)    
-    return jsonify({ 'count': len(posts), 'data': posts })
+        return jsonify({ 'message': 'Unable to fetch resources.' }), status.HTTP_500_INTERNAL_SERVER_ERROR 
+    resources = json.loads(res.content)    
+    return jsonify({ 'name': RESOURCE_NAME, 'count': len(resources), 'data': resources })
